98a04c410b3b9f5e0555e3618f7cff32787c94ba
TLCurd <TLCurdChiro@gmail.com>
2022-04-07 16:03:40 -0600

    add show action

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,18 @@
 <script>
 import axios from "axios";
+// import { response } from "express";
 export default {
   data: function () {
     return {
       message: "Check out these fruits!",
       fruits: [],
-      newFruitParams: {}
+      newFruit: {
+        name: "",
+        color: "",
+        description: "",
+        image: ""
+      },
+      currentFruit: {}
     };
   },
   created: function () {
@@ -15,16 +22,24 @@ export default {
   methods: {
     fruitsIndex: function () {
       console.log('in fruits...');
-      axios.get(`http://localhost:3000/fruits.json`).then(response => {
+      axios.get(`/fruits.json`).then(response => {
         console.log(response.data);
         this.fruits = response.data;
       });
     },
     createFruit: function () {
       console.log('creating fruits...');
-      axios.post(`http://localhost:3000/fruits.json`)
+      axios.post(`http://localhost:3000/fruits.json`, this.newFruit).then(response => {
+        console.log(response.data);
+        this.fruits.push(response.data);
+      })
+    },
+    showFruit: function (theFruit) {
+      console.log(theFruit);
+      document.querySelector("#fruit-details").showModal();
+      this.currentFruit = theFruit;
     }
-  },
+  }
 };
 </script>
 
@@ -38,29 +53,53 @@ export default {
       {{ fruit.id }}.
       <b>{{ fruit.name }}</b>
       <br />
+      <br />
       <img v-bind:src="fruit.image" />
+      <br />
+      <button v-on:click="showFruit(theFruit)">Let's get fruity</button>
       <hr />
     </div>
     <br />
     <p>
       Name:
-      <input v-model="newFruitParams.name" />
+      <input type="text" v-model="newFruit.name" />
     </p>
     <p>
       Color:
-      <input v-model="newFruitParams.color" />
+      <input type="text" v-model="newFruit.color" />
     </p>
     <p>
       Description:
-      <input v-model="newFruitParams.description" />
+      <input type="text" v-model="newFruit.description" />
     </p>
     <p>
       Image:
-      <input v-model="newFruitParams.image" />
+      <input type="text" v-model="newFruit.image" />
     </p>
     <button v-on:click="createFruit()">Add a new fruit!</button>
 
-    <button v-on:click="showProduct()">Let's get fruity</button>
+    <dialog id="fruit-details">
+      <form method="dialog">
+        <h1>Fruit info</h1>
+        <p>
+          Name:
+          <input type="text" v-model="currentFruit.name" />
+        </p>
+        <p>
+          Color:
+          <input type="text" v-model="currentFruit.color" />
+        </p>
+        <p>
+          Description:
+          <input type="text" v-model="currentFruit.description" />
+        </p>
+        <p>
+          Image:
+          <input type="text" v-model="currentFruit.image" />
+        </p>
+        <button>Close</button>
+      </form>
+    </dialog>
   </div>
 </template>
 
