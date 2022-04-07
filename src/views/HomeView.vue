9269fbfbad9259e3de1816a0741ced55a8c2dff1
TLCurd <TLCurdChiro@gmail.com>
2022-04-07 15:01:45 -0600

    add fruitsIndex, formats, createfruits

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,71 @@
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      message: "Check out these fruits!",
+      fruits: [],
+      newFruitParams: {}
+    };
+  },
+  created: function () {
+    console.log('in create');
+    this.fruitsIndex();
+  },
+  methods: {
+    fruitsIndex: function () {
+      console.log('in fruits...');
+      axios.get(`http://localhost:3000/fruits.json`).then(response => {
+        console.log(response.data);
+        this.fruits = response.data;
+      });
+    },
+    createFruit: function () {
+      console.log('creating fruits...');
+      axios.post(`http://localhost:3000/fruits.json`)
+    }
+  },
+};
+</script>
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <hr />
+    <br />
+
+    <div v-for="fruit in fruits">
+      {{ fruit.id }}.
+      <b>{{ fruit.name }}</b>
+      <br />
+      <img v-bind:src="fruit.image" />
+      <hr />
+    </div>
+    <br />
+    <p>
+      Name:
+      <input v-model="newFruitParams.name" />
+    </p>
+    <p>
+      Color:
+      <input v-model="newFruitParams.color" />
+    </p>
+    <p>
+      Description:
+      <input v-model="newFruitParams.description" />
+    </p>
+    <p>
+      Image:
+      <input v-model="newFruitParams.image" />
+    </p>
+    <button v-on:click="createFruit()">Add a new fruit!</button>
+
+    <button v-on:click="showProduct()">Let's get fruity</button>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+<style>
+img {
+  width: 300px;
 }
-</script>
+</style>
